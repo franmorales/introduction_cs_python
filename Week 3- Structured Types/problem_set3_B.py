e7d8642dbf7e6d270bd0a667fe2978ff5439c4ec
franmorales <francisco.morales0149@gmail.com>
2018-09-25 21:05:52 -0600

    Update problem_set3_B.py

--- Week 3- Structured Types/problem_set3_B.py
+++ Week 3- Structured Types/problem_set3_B.py
@@ -1,1 +1,16 @@
-print 'Hello Part b'
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    finalString  = ''
+    duplicateLetters = lettersGuessed[:]
+    for char in secretWord:
+        if char in duplicateLetters:
+            finalString += char
+            #duplicateLetters.remove(char)
+        else:
+            finalString += '_ '
+    return finalString
