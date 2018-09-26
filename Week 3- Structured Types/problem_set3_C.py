224207e859212dfdcc3bee4be07bac03feab0fe0
franmorales <francisco.morales0149@gmail.com>
2018-09-25 21:17:37 -0600

    Update problem_set3_C.py

--- Week 3- Structured Types/problem_set3_C.py
+++ Week 3- Structured Types/problem_set3_C.py
@@ -1,1 +1,10 @@
-print 'Parte C'
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    dasABC = list('abcdefghijklmnopqrstuvwxyz')
+    for ele in lettersGuessed:
+        dasABC.remove(ele)
+    return ''.join(map(str, dasABC))
