0e2d2a0dca911c2a9befcbafdc9fab456402b17a
franmorales <francisco.morales0149@gmail.com>
2018-09-25 20:13:42 -0600

    Update problem_set3_A.py

--- Week 3- Structured Types/problem_set3_A.py
+++ Week 3- Structured Types/problem_set3_A.py
@@ -1,1 +1,14 @@
-print 'hello world'
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    boolString  = ''
+    duplicateLetters = lettersGuessed[:]
+    for char in secretWord:
+        if char in duplicateLetters:
+            boolString += char
+            duplicateLetters.remove(char)
+    return boolString == secretWord
